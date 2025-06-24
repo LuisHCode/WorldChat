a972b1e879446a7897de6e06803c781ac202d687
LuisHCode <yolothelife95@gmail.com>
2025-06-24 14:45:31 -0600

    Se ha agregado el sp_EliminarParticipante, ademas de que ahora en Participante.py está el metodo para eliminar un participante, en este se necesita un id usuario y id admin, ya que solo el administrador lo puede eliminar

--- logica/controllers/Participante.py
+++ logica/controllers/Participante.py
@@ -46,8 +46,53 @@ async def agregar_participante(
             raise HTTPException(status_code=409, detail="El chat no existe.")
         if "50006" in mensaje:
             raise HTTPException(status_code=409, detail="El usuario no existe.")
-        if "50008" in mensaje:
+        if "50007" in mensaje:
             raise HTTPException(status_code=409, detail="El usuario ya es parte del chat.")
+        else:
+            raise HTTPException(status_code=500, detail="Error de base de datos: " + mensaje)
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error inesperado: " + str(e))
+
+@participante_router.post("/eliminar/{id_chat}")
+async def eliminar_participante(
+    id_chat: int,
+    request: Request,
+    response: Response,
+    db=Depends(s.obtener_conexion_sqlserver),
+):
+    body = await request.json()
+    # SQL con parámetros nombrados
+    sql = text(
+        """
+        EXEC sp_EliminarParticipante 
+            @id_chat = :id_chat, 
+            @id_usuario = :id_usuario,
+            @id_admin = :id_admin 
+    """
+    )
+    try:
+        with db.begin() as trans:
+            result = db.execute(sql, {
+                "id_chat": id_chat,
+                "id_usuario": body.get("id_usuario"),
+                "id_admin": body.get("id_admin")
+            })
+
+            return JSONResponse(status_code=201, content={"message": "Participante eliminado exitosamente."})
+
+    except DBAPIError as e:
+        mensaje = str(e.orig)
+
+        # 👇 Detectar error lanzado por el SP (por ejemplo THROW 50004)
+        if "50005" in mensaje:
+            raise HTTPException(status_code=409, detail="El chat no existe.")
+        if "50006" in mensaje:
+            raise HTTPException(status_code=409, detail="El usuario no existe.")
+        if "50007" in mensaje:
+            raise HTTPException(status_code=409, detail="El usuario no es parte del chat.")
+        if "50008" in mensaje:
+            raise HTTPException(status_code=409, detail="El Usuario no es administrador.")
         else:
             raise HTTPException(status_code=500, detail="Error de base de datos: " + mensaje)
 
